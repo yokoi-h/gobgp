48f8c93df0c11ef5672f5739b908eb15d7a7e190
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2015-08-20 21:20:24 +0900

    mrt

--- tools/grpc_python/mrt_dump.py
+++ tools/grpc_python/mrt_dump.py
@@ -1,29 +1,49 @@
-
 import gobgp_pb2
 import sys
+import netaddr
 
 _TIMEOUT_SECONDS = 10
+AFI_IP = 1
+AFI_IP6 = 2
+SAFI_UNICAST = 1
 
 
-def run(gobgpd):
+def check_address_family(neighbor):
+    ip = netaddr.IPAddress(neighbor)
+    if ip.version == 6:
+        return AFI_IP6 << 16 | SAFI_UNICAST
+    else:
+        return AFI_IP << 16 | SAFI_UNICAST
 
+def run(gobgpd, resource, args):
     with gobgp_pb2.early_adopter_create_Grpc_stub(gobgpd, 8080) as stub:
-        resource = sys.argv[2]
-        neighbor = ""
+
         if resource == "global":
-            res = 0
+            interval = 0
+            if len(args) > 0:
+                interval = int(args[0])
+
+            a = gobgp_pb2.MrtArguments(resource=0, interval=interval)
+
         elif resource == "neighbor":
-            res = 1
-            neighbor = sys.argv[3]
+            neighbor_address = args[0]
+            interval = 0
+            rf = check_address_family(neighbor_address)
+            if len(args) > 1:
+                interval = int(args[1])
+
+            a = gobgp_pb2.MrtArguments(resource=1, rf=rf,
+                                       neighbor_address=neighbor_address,
+                                       interval=interval)
         else:
             print("unknown resource type: %s" % resource)
 
-        arg = gobgp_pb2.MrtArguments(resource=res, rf=4, neighbor_address=neighbor)
-        dumps = stub.GetMrt(arg, _TIMEOUT_SECONDS)
+        dumps = stub.GetMrt(a, _TIMEOUT_SECONDS)
         for dump in dumps:
             print(dump.data)
 
 
 if __name__ == '__main__':
     gobgpd = sys.argv[1]
-    run(gobgpd)
+    resource = sys.argv[2]
+    run(gobgpd, resource, sys.argv[3:])
