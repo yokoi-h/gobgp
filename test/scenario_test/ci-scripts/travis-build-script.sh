e0b284079f46f296e941f66abe630110514d0274
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2016-03-24 18:39:51 +0900

    cat google_run_shutdown_scripts.log

--- test/scenario_test/ci-scripts/travis-build-script.sh
+++ test/scenario_test/ci-scripts/travis-build-script.sh
@@ -17,6 +17,7 @@ echo "--------------------------------------------------------------------------
 sudo ls -al /var/log/upstart
 sudo cat /var/log/upstart/network-interface-eth0.log
 sudo ip a
+sudo cat /var/log/upstart/google_run_shutdown_scripts.log
 echo "--------------------------------------------------------------------------------"
 
 export GOBGP_IMAGE=gobgp
